760b7456de344adc14c16faf8a53f3f715a999dd
Gaurav Joshi <121346271+Gauravjoshi08X@users.noreply.github.com>
2026-02-03 22:46:44 +0545

    Update main.py

--- main.py
+++ main.py
@@ -27,5 +27,7 @@ def terms():
     <p>We may suspend accounts for abuse.</p>
     """)
 
-if __name__=="__main__":
-    app.run()
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
+
